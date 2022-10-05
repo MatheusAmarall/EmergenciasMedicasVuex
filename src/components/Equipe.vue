27ed93556eee3ca79a20d6d257ca592f0ff1b044
Matheus Amaral <m.amaral8998@gmail.com>
2022-10-05 13:45:48 -0300

    estudos vuex

---
+++ src/components/Equipe.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-2" v-text="dados.enfermeiro" />
+        <div class="col-2" v-text="dados.socorrista" />
+        <div class="col-2" v-text="dados.medico" />
+        <div class="col-2" v-text="dados.carro" />
+        <div class="col-2" v-text="dados.telefone" />
+        <div class="col-2" v-text="dados.kitDeReanimacao" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Equipe',
+    props: {
+        dados: Object
+    }
+}
+</script>
